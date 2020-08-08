cf7521e26147f7887e2c3faa44499e3a22693145
TrentBrunson <66187531+TrentBrunson@users.noreply.github.com>
2020-08-07 19:55:11 -0500

    added home page and precip file page

--- app.py
+++ app.py
@@ -1,12 +1,4 @@
 #%%
-from flask import Flask
-app = Flask(__name__)
-
-#%%
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
-#%%
 import datetime as dt
 import numpy as np
 import pandas as pd
@@ -25,19 +17,38 @@ Base.prepare(engine, reflect = True)
 Measurement = Base.classes.measurement
 Station = Base.classes.station
 session = Session(engine)
+
 #%%
 # activate flask
 app = Flask(__name__)
 
 # %%
-@ app.route('/')
+@app.route('/')
 def welcome():
     return(
-        '''
-        Welcome to the Climate Analysis API!
-        Available Routes:
-        /api/v1.0/precipitation
-        /api/v1.0/stations
-        /api/v1.0/tobs
-        /api/v1.0/temp/start/end
+    '''
+    Welcome to the Climate Analysis API!
+    Available Routes:
+    /api/v1.0/precipitation
+    /api/v1.0/stations
+    /api/v1.0/tobs
+    /api/v1.0/temp/start/end
     ''')
+#%%
+@app.route("/api/v1.0/precipitation")
+def precipitation():
+    prev_year = dt.date(2017, 8, 23) - dt.timedelta(days = 365)
+    precipitation = session.query(Measurement.date, Measurement.prcp).filter(
+        Measurement.date >= prev_year).all()
+    precip = {date: prcp for date, prcp in precipitation}
+    return jsonify(precip)
+# %%
+
+
+# %%
+
+#%%
+if __name__ == "__main__":
+   app.run()
+
+# %%
