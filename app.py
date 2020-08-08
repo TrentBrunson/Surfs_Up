c5eedb611fdfa2e364c147a0034f1675ab6f1e71
TrentBrunson <66187531+TrentBrunson@users.noreply.github.com>
2020-08-07 19:03:59 -0500

    python code

--- app.py
+++ app.py
@@ -1,5 +1,3 @@
-# %%
-import pandas as pd
 #%%
 from flask import Flask
 app = Flask(__name__)
@@ -9,3 +7,26 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello World!'
 #%%
+import datetime as dt
+import numpy as np
+import pandas as pd
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+from flask import Flask, jsonify
+#%%
+# create engine for connection and reflect the DB
+engine = create_engine("sqlite:///hawaii.sqlite")
+Base = automap_base()
+Base.prepare(engine, reflect = True)
+#%%
+# create variable to hold references for tables & create session link
+Measurement = Base.classes.measurement
+Station = Base.classes.station
+session = Session(engine)
+#%%
+# activate flask
+app = Flask(__name__)
+
+# %%
